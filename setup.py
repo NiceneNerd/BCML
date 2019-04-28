d65d7ac0547c824b74a591142064f28d09273da1
Caleb Smith <calebdixonsmith@gmail.com>
2019-04-28 08:31:00 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/README.md", "r") as readme:
 
 setup(
     name='bcml',
-    version='1.0',
+    version='1.01',
     author='NiceneNerd',
     author_email='macadamiadaze@gmail.com',
     description='A mod manager for The Legend of Zelda: Breath of the Wild on Cemu',
