f393a886496ab6e470fafb994748cc9886c7fe9e
Caleb Smith <calebdixonsmith@gmail.com>
2019-07-06 09:44:03 -0500

    Version bump

--- bcml/__init__.py
+++ bcml/__init__.py
@@ -68,7 +68,7 @@ def main():
 
     print('##############################################')
     print('##    Breath of the Wild Cemu Mod Loader    ##')
-    print('##              Version 1.21                ##')
+    print('##              Version 1.22                ##')
     print('##------------------------------------------##')
     print('##     (c) 2019 Nicene Nerd - GPLv3+        ##')
     print('##  7z.exe (c) 2019 Ignor Pavolv - LGPLv3+  ##')
