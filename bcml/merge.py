3402dade5fcd24ea2515ec83c5be8ebfa495bca7
Caleb Smith <calebdixonsmith@gmail.com>
2019-08-28 10:41:18 -0500

    Formatting

--- bcml/merge.py
+++ bcml/merge.py
@@ -335,7 +335,8 @@ def deep_merge(verbose: bool = False, wait_rstb: bool = False, only_these: List[
             if modded_sarcs:
                 modded_sarc_files.update(modded_sarcs)
     (util.get_master_modpack_dir() / 'logs').mkdir(parents=True, exist_ok=True)
-    with Path(util.get_master_modpack_dir() / 'logs' / 'rstb.log').open('w', encoding='utf-8') as r_file:
+    with Path(util.get_master_modpack_dir() / 'logs' / 'rstb.log')\
+         .open('w', encoding='utf-8') as r_file:
         r_file.write('name,rstb\n')
         modded_files.update(modded_sarc_files)
         for file in modded_files:
@@ -351,7 +352,8 @@ def deep_merge(verbose: bool = False, wait_rstb: bool = False, only_these: List[
     if not wait_rstb:
         bcml.rstable.generate_master_rstb()
 
-    with (util.get_master_modpack_dir() / 'logs' / 'deepmerge.log').open('w', encoding='utf-8') as l_file:
+    with (util.get_master_modpack_dir() / 'logs' / 'deepmerge.log')\
+         .open('w', encoding='utf-8') as l_file:
         for file_type in diffs:
             for file in diffs[file_type]:
                 l_file.write(f'{file}\n')
