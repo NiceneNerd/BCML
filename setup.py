a8a6d849b8d33e5bb5054cc7b8f4af3c932b89b1
Caleb Smith <calebdixonsmith@gmail.com>
2019-11-20 15:57:13 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if not compiled_path.exists():
 
 setup(
     name='bcml',
-    version='2.6.0',
+    version='2.6.1',
     author='NiceneNerd',
     author_email='macadamiadaze@gmail.com',
     description='A mod manager for The Legend of Zelda: Breath of the Wild on Cemu',
