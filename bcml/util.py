81657e1199d7cbf628d39a0348f61161de64bb4a
Caleb Smith <calebdixonsmith@gmail.com>
2020-05-18 15:43:22 -0500

    Allow using system 7zip on Linux

--- bcml/util.py
+++ bcml/util.py
@@ -1242,4 +1242,21 @@ class Messager:
 if system() == "Windows":
     ZPATH = str(get_exec_dir() / "helpers" / "7z.exe")
 else:
-    ZPATH = str(get_exec_dir() / "helpers" / "7z")
+    ZPATH = _which("7z") or str(get_exec_dir() / "helpers" / "7z")
+
+
+def _which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
