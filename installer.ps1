0e014c3e3b876f4377fa2e60ea57c46b53395c23
Caleb Smith <macadamiadaze@gmail.com>
2021-01-05 06:23:49 -0600

    Installer tweaks and fixes

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,11 @@
 $ErrorActionPreference = "Stop"
+
+function PromptQuit {
+    Write-Host "Press any key to exit..."
+    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+    exit
+}
+
 Write-Host "███████████████████████████████████████" -ForegroundColor DarkGray -NoNewLine -BackgroundColor DarkGray
 Write-Host "  ______  _____ ___  ___ _" -ForegroundColor White
 
@@ -88,65 +95,102 @@ try {
     else {
         Write-Host "Python 3.7+ already installed, moving on..."
         $Python = (Get-Command python).Source
-    }
+    }    
+    try {
+        Get-Command bcml | Out-Null
+        Write-Host "BCML is already installed, no need for this."
+        PromptQuit
+    } catch {}
 }
 catch {
     Write-Host "Looks like you need Python! Downloading Python 3.7.9..."
-    (New-Object Net.WebClient).DownloadFile("https://www.python.org/ftp/python/3.7.9/python-3.7.9-embed-amd64.zip", "$env:temp\python.zip")
-    Write-Host "Downloaded Python. Extracting..."
-    Expand-Archive -Path "$env:temp\python.zip" -DestinationPath "$home\.python"
-    $Python = "$home\.python\python.exe"
+    try {
+        (New-Object Net.WebClient).DownloadFile("https://www.python.org/ftp/python/3.7.9/python-3.7.9-embed-amd64.zip", "$env:temp\python.zip")
+    }
+    catch {
+        Write-Error "Could not download Python. Please check your Internet connection."
+        PromptQuit
+    }
+    Write-Host "Downloaded Python. Where would you like it to be installed?"
+    Write-Host "(If you leave this blank, it will default to %USERPROFILE%\.python)"
+    Write-Host "> " -NoNewline
+    $PythonDir = Read-Host
+    if ([String]::IsNullOrWhiteSpace($PythonDir)) {
+        $PythonDir = "$home\.python"
+    }
+    try {
+        Expand-Archive -Path "$env:temp\python.zip" -DestinationPath "$PythonDir" -Force
+    }
+    catch {
+        Write-Error "There was a problem extracting the Python package."
+        PromptQuit
+    }
+    $Python = "$PythonDir\python.exe"
     Write-Host "Setting up pip..."
-    (New-Object Net.WebClient).DownloadFile("https://bootstrap.pypa.io/get-pip.py", "$home\.python\get-pip.py") | Out-Null
-    & $Python "$home\.python\get-pip.py" --disable-pip-version-check --no-warn-script-location | Out-Null
-    Set-Content -Path "$home\.python\python37._pth" -Value "python37.zip`n.`n.\Lib`n.\Lib\site-packages`n.\DLLs`n"
+    (New-Object Net.WebClient).DownloadFile("https://bootstrap.pypa.io/get-pip.py", "$PythonDir\get-pip.py") | Out-Null
+    & $Python "$PythonDir\get-pip.py" --disable-pip-version-check --no-warn-script-location | Out-Null
+    Set-Content -Path "$PythonDir\python37._pth" -Value "python37.zip`n.`n.\Lib`n.\Lib\site-packages`n.\DLLs`n"
     Write-Host "Python is all set!"
 }
 
-Write-Host "Installing latest BCML from PyPI..."
-& $Python -m pip install bcml --disable-pip-version-check --no-warn-script-location | Out-Null
+try {
+    Write-Host "Installing latest BCML from PyPI..."
+    & $Python -m pip install bcml --disable-pip-version-check --no-warn-script-location | Out-Null
+}
+catch {
+    Write-Error "BCML did not install successfully from PyPI."
+    PromptQuit
+}
 
-Write-Host "Creating shortcuts..."
-New-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs" -Name "BCML" -ItemType "directory" -ErrorAction SilentlyContinue | Out-Null
-$ShowOutput = (& $Python -m pip show bcml).Split("`n")[7].Split()[-1]
-$InstallDir = "$ShowOutput\bcml"
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk")
-$Shortcut.TargetPath = $Python.Replace("python.exe", "pythonw.exe")
-$Shortcut.Arguments = "-m bcml"
-$Shortcut.Description = "BCML"
-$Shortcut.IconLocation = "$InstallDir\data\bcml.ico"
-$Shortcut.Save()
-Copy-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk" "$home\Desktop\BCML.lnk"
-
-$UpdateShortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Update BCML.lnk")
-$UpdateShortcut.TargetPath = $Python
-$UpdateShortcut.Arguments = "-m pip install -U bcml"
-$UpdateShortcut.Description = "Update BCML"
-$UpdateShortcut.Save()
-
-Set-Content -Path "$InstallDir\Uninstall.ps1" -Value @"
-Write-Host "Uninstalling BCML..."
-& $Python -m pip uninstall bcml -y | Out-Null
-Remove-Item -Path "`$home\Desktop\BCML.lnk"
-Remove-Item -Path "`$home\.python" -ErrorAction SilentlyContinue -Recurse -Force | Out-Null
-Write-Host "BCML uninstalled successfully"
-Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk"
-Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Uninstall.lnk"
-Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Update BCML.lnk"
-Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML" -Force -ErrorAction SilentlyContinue
-Write-Host "Press any key to exit..."
-`$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+try {
+    Write-Host "Creating shortcuts..."
+    New-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs" -Name "BCML" -ItemType "directory" -ErrorAction SilentlyContinue | Out-Null
+    $ShowOutput = (& $Python -m pip show bcml).Split("`n")[7].Split()[-1]
+    $InstallDir = "$ShowOutput\bcml"
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk")
+    $Shortcut.TargetPath = $Python.Replace("python.exe", "pythonw.exe")
+    $Shortcut.Arguments = "-m bcml"
+    $Shortcut.Description = "BCML"
+    $Shortcut.IconLocation = "$InstallDir\data\bcml.ico"
+    $Shortcut.Save()
+    $Desktop = Read-Host "Do you want to create a desktop shortcut? (y/n)"
+    if ($Desktop -contains "y") {
+        Copy-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk" "$home\Desktop\BCML.lnk"
+    }
+    
+    $UpdateShortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Update BCML.lnk")
+    $UpdateShortcut.TargetPath = $Python
+    $UpdateShortcut.Arguments = "-m pip install -U bcml"
+    $UpdateShortcut.Description = "Update BCML"
+    $UpdateShortcut.Save()
+    
+    Set-Content -Path "$InstallDir\Uninstall.ps1" -Value @"
+    Write-Host "Uninstalling BCML..."
+    & $Python -m pip uninstall bcml -y | Out-Null
+    Write-Host "BCML uninstalled successfully"
+    Remove-Item -Path "`$home\Desktop\BCML.lnk" -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\BCML.lnk"
+    Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Uninstall.lnk"
+    Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Update BCML.lnk"
+    Remove-Item -Path "`$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML" -Force -ErrorAction SilentlyContinue
+    Write-Host "Press any key to exit..."
+    `$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
 "@
-$UninstallShortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Uninstall.lnk")
-$UninstallShortcut.TargetPath = (Get-Command powershell).Source
-$UninstallShortcut.Arguments = "$InstallDir\Uninstall.ps1"
-$UninstallShortcut.Description = "Uninstall"
-$UninstallShortcut.Save()
+    $UninstallShortcut = $WshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\BCML\Uninstall.lnk")
+    $UninstallShortcut.TargetPath = (Get-Command powershell).Source
+    $UninstallShortcut.Arguments = "$InstallDir\Uninstall.ps1"
+    $UninstallShortcut.Description = "Uninstall"
+    $UninstallShortcut.Save()
+}
+catch {
+    Write-Host "There was a problem creating shortcuts. BCML is still there; you can run it with:"
+    Write-Host "$Python -m bcml"
+    Write-Host "Feel free to make your own shortcut to this command."
+}
 
 Write-Host "BCML has been installed! Go ahead and launch it from the shortcut if you wish.`n"
 Write-Host "Just a reminder: I have a full time job, a Masters program, and 6 children."
 Write-Host "So if you like what I do, consider supporting my Patreon (link above)."
 Write-Host "It's what convinces my wife (partially) this is all worth the time.`n"
-Write-Host "Press any key to exit..."
-$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+PromptQuit
