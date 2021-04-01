89fad920aec0a5ff23b5de2b83b1fd93187a3e9d
Caleb Smith <caleb@myrvmail.com>
2021-04-01 17:00:17 -0500

    Mainfield static merger basically finished

--- bcml/mergers/mainstatic.py
+++ bcml/mergers/mainstatic.py
@@ -20,26 +20,18 @@ from bcml import util, mergers
 
 STATIC_PATH = Path("Map", "MainField", "Static.smubin")
 
-KEY_MAP = {
-    "DLCRestartPos": "UniqueName",
-    "FldObj_DLC_ShootingStarCollaborationAnchor": "collaboSSFalloutFlagName",
-    "KorokLocation": "Flag",
-    "LocationMarker": "SaveFlag",
-    "LocationPointer": "SaveFlag",
-    "NonAutoGenArea": "Translate",
-    "NonAutoPlacement": "Translate",
-    "RoadNpcRestStation": "Translate",
-    "StartPos": "PosName",
-    "StaticGrudgeLocation": "Translate",
-    "TargetPosMarker": "UniqueName",
-    "TeraWaterDisable": "Translate",
-    "TerrainHideCenterTag": "Translate",
-}
-
 
 @lru_cache(1024)
 def key_from_coords(x: float, y: float, z: float) -> str:
     return str(ceil(x)) + str(ceil(y)) + str(ceil(z))
+
+
+def get_id(item: Hash) -> str:
+    return key_from_coords(
+        item["Translate"]["X"].v,
+        item["Translate"]["Y"].v,
+        item["Translate"]["Z"].v,
+    )
 
 
 class MainfieldStaticMerger(mergers.Merger):
@@ -75,20 +67,16 @@ class MainfieldStaticMerger(mergers.Merger):
             stock_static_path = util.get_game_file("Map/MainField/Static.smubin")
         else:
             return None
-        stock_static = Hash.from_binary(util.decompress(stock_static_path.read_bytes()))
-        mod_static = Hash.from_binary(util.decompress(static_path.read_bytes()))
+        stock_static = oead.byml.from_binary(
+            util.decompress(stock_static_path.read_bytes())
+        )
+        mod_static = oead.byml.from_binary(util.decompress(static_path.read_bytes()))
         diffs = Hash()
-        get_trans = itemgetter("Translate")
-        get_trans_id = lambda x: key_from_coords(x.x.v, x.y.v, x.z.v)
-        for cat, key in KEY_MAP.items():
-            if key == "Translate":
-                id_getter = lambda x: get_trans_id(get_trans(x))
-            else:
-                id_getter = itemgetter(key)
+        for cat in stock_static:
             if cat not in stock_static:
                 continue
-            stock_items = {id_getter(item): item for item in stock_static[cat]}
-            mod_items = {id_getter(item): item for item in mod_static[cat]}
+            stock_items = {get_id(item): item for item in stock_static[cat]}
+            mod_items = {get_id(item): item for item in mod_static[cat]}
             diffs[cat] = Hash(
                 {
                     item_id: item
@@ -110,3 +98,74 @@ class MainfieldStaticMerger(mergers.Merger):
             (mod_dir / "logs" / self._log_name).write_text(
                 oead.byml.to_text(diff_material), encoding="utf-8"
             )
+
+    def get_mod_diff(self, mod: util.BcmlMod):
+        diff = oead.byml.Hash()
+        if self.is_mod_logged(mod):
+            diff = oead.byml.from_text(
+                (mod.path / "logs" / self._log_name).read_text("utf-8")
+            )
+        for opt in {d for d in (mod.path / "options").glob("*") if d.is_dir()}:
+            if (opt / "logs" / self._log_name).exists():
+                util.dict_merge(
+                    diff,
+                    oead.byml.from_text(
+                        (opt / "logs" / self._log_name).read_text("utf-8")
+                    ),
+                    overwrite_lists=True,
+                )
+        return diff
+
+    def get_all_diffs(self):
+        diffs = []
+        for m in util.get_installed_mods():
+            diff = self.get_mod_diff(m)
+            if diff:
+                diffs.append(diff)
+        return diffs
+
+    def consolidate_diffs(self, diffs):
+        if not diffs:
+            return {}
+        all_diffs = oead.byml.Hash()
+        for diff in diffs:
+            util.dict_merge(all_diffs, diff, overwrite_lists=True)
+        return all_diffs
+
+    @util.timed
+    def perform_merge(self):
+        diffs = self.consolidate_diffs(self.get_all_diffs())
+        output: Path
+        static_path: Path
+        try:
+            util.get_aoc_dir()
+            output = (
+                util.get_master_modpack_dir()
+                / util.get_dlc_path()
+                / ("0010" if util.get_settings("wiiu") else "")
+                / STATIC_PATH
+            )
+            static_path = util.get_game_file("Map/MainField/Static.smubin", aoc=True)
+        except FileNotFoundError:
+            output = (
+                util.get_master_modpack_dir() / util.get_content_path() / STATIC_PATH
+            )
+            static_path = util.get_game_file("Map/MainField/Static.smubin")
+        if not diffs:
+            try:
+                output.unlink()
+            except:
+                pass
+            return
+        stock_static = oead.byml.from_binary(util.decompress(static_path.read_bytes()))
+        merged = Hash()
+        for cat in stock_static:
+            items = {get_id(item): item for item in stock_static[cat]}
+            util.dict_merge(items, diffs[cat])
+            merged[cat] = Array([item for _, item in items.items() if "remove" not in item])
+        output.parent.mkdir(parents=True, exist_ok=True)
+        output.write_bytes(
+            util.compress(
+                oead.byml.to_binary(merged, big_endian=util.get_settings("wiiu"))
+            )
+        )
