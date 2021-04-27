d16685d9cbbae961dfe6fb07e136890a9e7318f0
Caleb Smith <macadamiadaze@gmail.com>
2021-04-27 13:36:47 -0500

    Improve wide UI

--- bcml/assets/src/js/ModInfo.jsx
+++ bcml/assets/src/js/ModInfo.jsx
@@ -1,7 +1,8 @@
 import { Badge, Button, Spinner } from "react-bootstrap";
-import ReactMarkdown from "react-markdown";
-import React from "react";
+
 import ModContext from "./Context.jsx";
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
 class ModInfo extends React.Component {
     static contextType = ModContext;
@@ -57,139 +58,142 @@ class ModInfo extends React.Component {
     render() {
         return (
             <>
-                <div className="mod-header">
-                    <img
-                        src={
-                            (this.props.mod &&
-                                (this.props.mod.name.includes("NSFW") ||
-                                this.props.mod.name.includes("THICC")
-                                    ? "https://i.imgur.com/AfedhPe.png"
-                                    : this.state.image
-                                    ? "data:image/*;charset=utf-8;base64," +
-                                      this.state.image
-                                    : null)) ||
-                            "logo-smaller.png"
-                        }
-                        className="mod-preview"
-                    />
-                    <div className="mod-preview-shadow"></div>
-                    <div className="mod-title">
-                        <h1>
-                            {this.props.mod ? (
-                                this.state.url ? (
-                                    <a href={this.state.url} target="_blank">
-                                        {this.props.mod.name}
-                                    </a>
+                <div className="mod-content">
+                    <div className="mod-header">
+                        <img
+                            src={
+                                (this.props.mod &&
+                                    (this.props.mod.name.includes("NSFW") ||
+                                    this.props.mod.name.includes("THICC")
+                                        ? "https://i.imgur.com/AfedhPe.png"
+                                        : this.state.image
+                                        ? "data:image/*;charset=utf-8;base64," +
+                                          this.state.image
+                                        : null)) ||
+                                "logo-smaller.png"
+                            }
+                            className="mod-preview"
+                        />
+                        <div className="mod-preview-shadow"></div>
+                        <div className="mod-title">
+                            <h1>
+                                {this.props.mod ? (
+                                    this.state.url ? (
+                                        <a href={this.state.url} target="_blank">
+                                            {this.props.mod.name}
+                                        </a>
+                                    ) : (
+                                        this.props.mod.name
+                                    )
                                 ) : (
-                                    this.props.mod.name
-                                )
-                            ) : (
-                                "Welcome to BCML"
+                                    "Welcome to BCML"
+                                )}
+                            </h1>
+                            {this.props.mod && this.props.mod.disabled && (
+                                <Badge variant="danger">DISABLED</Badge>
                             )}
-                        </h1>
-                        {this.props.mod && this.props.mod.disabled && (
-                            <Badge variant="danger">DISABLED</Badge>
+                        </div>
+                    </div>
+                    <ReactMarkdown className="mod-descrip">
+                        {this.props.mod
+                            ? this.state.desc || "No description"
+                            : "No mod selected"}
+                    </ReactMarkdown>
+                    <div className="mod-actions">
+                        <Button
+                            variant="primary"
+                            size="sm"
+                            title="Explore (Ctrl+X)"
+                            disabled={!this.props.mod || this.props.multi}
+                            onClick={() => this.props.onAction("explore")}>
+                            <i className="material-icons">folder_open</i>{" "}
+                            <span>Explore</span>
+                        </Button>
+                        {this.props.mod &&
+                            (this.props.mod.disabled ? (
+                                <Button
+                                    variant="success"
+                                    size="sm"
+                                    title="Enable (Ctrl+E)"
+                                    disabled={!this.props.mod}
+                                    onClick={() => this.props.onAction("enable")}>
+                                    <i className="material-icons">check_box</i>{" "}
+                                    <span>Enable</span>
+                                </Button>
+                            ) : (
+                                <Button
+                                    variant="warning"
+                                    size="sm"
+                                    title="Disable (Ctrl+D)"
+                                    disabled={!this.props.mod}
+                                    onClick={() => this.props.onAction("disable")}>
+                                    <i className="material-icons">block</i>{" "}
+                                    <span>Disable</span>
+                                </Button>
+                            ))}
+                        <Button
+                            variant="info"
+                            size="sm"
+                            title="Update"
+                            disabled={!this.props.mod || this.props.multi}
+                            onClick={() => this.props.onAction("update")}>
+                            <i className="material-icons">update</i> <span>Update</span>
+                        </Button>
+                        {this.state.processed && !this.context.settings?.strip_gfx && (
+                            <Button
+                                variant="secondary"
+                                size="sm"
+                                title="Reprocess (Ctrl+P)"
+                                disabled={!this.props.mod || this.props.multi}
+                                onClick={() => this.props.onAction("reprocess")}>
+                                <i className="material-icons">refresh</i>{" "}
+                                <span>Reprocess</span>
+                            </Button>
+                        )}
+                        <Button
+                            variant="danger"
+                            size="sm"
+                            title="Uninstall (Ctrl+U)"
+                            disabled={!this.props.mod}
+                            onClick={() => this.props.onAction("uninstall")}>
+                            <i className="material-icons">delete</i>{" "}
+                            <span>Uninstall</span>
+                        </Button>
+                    </div>
+                    <div className="mod-details">
+                        {this.props.mod ? (
+                            <>
+                                <span>
+                                    <strong>Priority:</strong> {this.props.mod.priority}
+                                </span>
+                                <span>
+                                    <strong>Changes:</strong>{" "}
+                                    {this.state.changes.join(", ")}
+                                </span>
+                                <span
+                                    style={{
+                                        whiteSpace: "nowrap",
+                                        maxWidth: "100%",
+                                        overflow: "hidden"
+                                    }}>
+                                    <strong>Path:</strong> {this.props.mod.path}
+                                </span>
+                                {this.state.url && (
+                                    <span>
+                                        <strong>URL:</strong>{" "}
+                                        <a href={this.state.url} target="_blank">
+                                            {this.state.url}
+                                        </a>
+                                    </span>
+                                )}
+                                <span>
+                                    <strong>Date:</strong> {this.props.mod.date}
+                                </span>
+                            </>
+                        ) : (
+                            "No mod selected"
                         )}
                     </div>
-                </div>
-                <ReactMarkdown className="mod-descrip">
-                    {this.props.mod
-                        ? this.state.desc || "No description"
-                        : "No mod selected"}
-                </ReactMarkdown>
-                <div className="mod-actions">
-                    <Button
-                        variant="primary"
-                        size="sm"
-                        title="Explore (Ctrl+X)"
-                        disabled={!this.props.mod || this.props.multi}
-                        onClick={() => this.props.onAction("explore")}>
-                        <i className="material-icons">folder_open</i>{" "}
-                        <span>Explore</span>
-                    </Button>
-                    {this.props.mod &&
-                        (this.props.mod.disabled ? (
-                            <Button
-                                variant="success"
-                                size="sm"
-                                title="Enable (Ctrl+E)"
-                                disabled={!this.props.mod}
-                                onClick={() => this.props.onAction("enable")}>
-                                <i className="material-icons">check_box</i>{" "}
-                                <span>Enable</span>
-                            </Button>
-                        ) : (
-                            <Button
-                                variant="warning"
-                                size="sm"
-                                title="Disable (Ctrl+D)"
-                                disabled={!this.props.mod}
-                                onClick={() => this.props.onAction("disable")}>
-                                <i className="material-icons">block</i>{" "}
-                                <span>Disable</span>
-                            </Button>
-                        ))}
-                    <Button
-                        variant="info"
-                        size="sm"
-                        title="Update"
-                        disabled={!this.props.mod || this.props.multi}
-                        onClick={() => this.props.onAction("update")}>
-                        <i className="material-icons">update</i> <span>Update</span>
-                    </Button>
-                    {this.state.processed && !this.context.settings?.strip_gfx && (
-                        <Button
-                            variant="secondary"
-                            size="sm"
-                            title="Reprocess (Ctrl+P)"
-                            disabled={!this.props.mod || this.props.multi}
-                            onClick={() => this.props.onAction("reprocess")}>
-                            <i className="material-icons">refresh</i>{" "}
-                            <span>Reprocess</span>
-                        </Button>
-                    )}
-                    <Button
-                        variant="danger"
-                        size="sm"
-                        title="Uninstall (Ctrl+U)"
-                        disabled={!this.props.mod}
-                        onClick={() => this.props.onAction("uninstall")}>
-                        <i className="material-icons">delete</i> <span>Uninstall</span>
-                    </Button>
-                </div>
-                <div className="mod-details">
-                    {this.props.mod ? (
-                        <>
-                            <span>
-                                <strong>Priority:</strong> {this.props.mod.priority}
-                            </span>
-                            <span>
-                                <strong>Changes:</strong>{" "}
-                                {this.state.changes.join(", ")}
-                            </span>
-                            <span
-                                style={{
-                                    whiteSpace: "nowrap",
-                                    maxWidth: "100%",
-                                    overflow: "hidden"
-                                }}>
-                                <strong>Path:</strong> {this.props.mod.path}
-                            </span>
-                            {this.state.url && (
-                                <span>
-                                    <strong>URL:</strong>{" "}
-                                    <a href={this.state.url} target="_blank">
-                                        {this.state.url}
-                                    </a>
-                                </span>
-                            )}
-                            <span>
-                                <strong>Date:</strong> {this.props.mod.date}
-                            </span>
-                        </>
-                    ) : (
-                        "No mod selected"
-                    )}
                 </div>
                 {this.state.loading && (
                     <div className="load-backdrop">
