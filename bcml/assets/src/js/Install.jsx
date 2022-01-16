b34bdcb34a4d75a9d0a19695ded2e35fc490f188
HGStone <85000440+HGStone@users.noreply.github.com>
2022-01-16 12:18:38 -0600

    Update Install.jsx

--- bcml/assets/src/js/Install.jsx
+++ bcml/assets/src/js/Install.jsx
@@ -68,7 +68,7 @@ class InstallModal extends React.Component {
                         Select a mod or mods to install (must be a BNP or
                         graphic pack). You can select a <code>.bnp</code>,{" "}
                         <code>.zip</code>, <code>.rar</code>, or{" "}
-                        <code>.7z</code> file or the <code>rules.txt</code> or
+                        <code>.7z</code> file or the <code>rules.txt</code> or{" "}
                         <code>info.json</code> of an unpacked mod.
                     </p>
                     <Button size="sm" onClick={() => this.browse()}>
