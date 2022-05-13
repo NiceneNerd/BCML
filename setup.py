67489c93b04ea230a56e4fe45e0a8607cac20228
Caleb Smith <caleb@myrvmail.com>
2022-05-13 10:39:56 -0500

    Restore `setup.py`

---
+++ setup.py
@@ -0,0 +1,37 @@
+# pylint: disable=invalid-name
+import os
+from sys import argv
+from pathlib import Path
+from setuptools import setup
+
+with open("docs/README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="bcml",
+    version="3.9.0-beta2",
+    author="NiceneNerd",
+    author_email="macadamiadaze@gmail.com",
+    description="A mod manager for The Legend of Zelda: Breath of the Wild",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NiceneNerd/BCML",
+    include_package_data=True,
+    packages=["bcml"],
+    package_dir={"bcml": "bcml"},
+    entry_points={
+        "gui_scripts": ["bcml = bcml.__main__:main"],
+        "console_scripts": [
+            "bcml-debug = bcml.__main__:main_debug",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires=">=3.7",
+    install_requires=Path("requirements.txt").read_text().splitlines(),
+    zip_safe=False,
+)
--- vbump.py
+++ vbump.py
@@ -4,6 +4,13 @@ import sys
 version = sys.argv[1]
 major, minor, patch = version.split(".")
 
+setup = Path("setup.py")
+setup_text = setup.read_text().splitlines()
+for i, line in enumerate(setup_text):
+    if "version=" in line:
+        setup_text[i] = f'    version="{version}",'
+setup.write_text("\n".join(setup_text))
+
 version_file = Path("bcml/__version__.py")
 version_file_text = version_file.read_text().splitlines()
 version_file_text[0:3] = [f"_MAJOR={major}", f"_MINOR={minor}", f'_PATCH="{patch}"']
