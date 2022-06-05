617a47d66de871a9cf5111024093bf9bd1b37372
Waikuteru <89609865+Waikuteru@users.noreply.github.com>
2022-06-05 02:09:07 +0200

    BFRES RSTB Fix - Custom Number Shrines Aoc
    With this fix, you can load into a custom number shrine in aoc directory without a crash.

--- bcml/mergers/rstable.py
+++ bcml/mergers/rstable.py
@@ -57,6 +57,8 @@ def calculate_size(
         size = getattr(calculate_size, "calculator").calculate_file_size_with_ext(
             data, wiiu=be, ext=ext, force=False
         )
+        if ext == ".bfres":
+            size += 1000
         if ext == ".baischedule":
             size += 100
         if ext == ".bdmgparam":
