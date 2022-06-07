e45a2c1ca78088c39e32caf5883097f2c64f1074
Caleb Smith <caleb@myrvmail.com>
2022-06-07 09:06:46 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("docs/README.md", "r") as readme:
 
 setup(
     name="bcml",
-    version="3.9.6",
+    version="3.9.7",
     author="NiceneNerd",
     author_email="macadamiadaze@gmail.com",
     description="A mod manager for The Legend of Zelda: Breath of the Wild",
